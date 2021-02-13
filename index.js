8928e276582670e6c9fc287c29771008c441abea
a96lex <a96lex@gmail.com>
2021-02-13 21:42:35 +0100

    context

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
+import React from "react";
 import { registerRootComponent } from "expo";
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-
-import React from "react";
+import { ContentProvider } from "./src/context/ContentContext";
+import { UserProvider } from "./src/context/UserContext";
 
 import App from "./App";
-import { UserProvider } from "./src/context/UserContext";
 
 export default function Index() {
   return (
     <UserProvider>
-      <App />
+      <ContentProvider>
+        <App />
+      </ContentProvider>
     </UserProvider>
   );
 }
