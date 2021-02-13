7a022f9896c14b7bf5b7c1bdbcb33a90ee3c5af1
a96lex <a96lex@gmail.com>
2021-02-13 21:43:33 +0100

    content context

--- src/context/Types.js
+++ src/context/Types.js
@@ -1,3 +1,4 @@
+// user
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
 export const LOG_IN_START = "LOG_IN_START";
 export const LOG_IN_FAIL = "LOG_IN_FAIL";
@@ -5,3 +6,9 @@ export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
 export const SIGN_UP_START = "SIGN_UP_START";
 export const SIGN_UP_FAIL = "SIGN_UP_FAIL";
 export const LOG_OFF = "LOG_OFF";
+
+// content
+export const VOLUNTEERING_LIST_SUCCESS = "VOLUNTEERING_LIST_SUCCESS";
+export const DONATION_LIST_SUCCESS = "DONATION_LIST_SUCCESS";
+export const GET_CENTER_SUCCESS = "GET_CENTER_SUCCESS";
+export const ANY_ERROR = "ANY_ERROR";
