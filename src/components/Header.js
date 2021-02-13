2769c52575d06e6aac4dad76fa03d2a113a03f95
a96lex <a96lex@gmail.com>
2021-02-13 21:43:16 +0100

    estilo e icono

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Header({ navigation }) {
   return (
     <>
       <View style={styles.header}>
-        <Text
-          onPress={() => navigation.toggleDrawer()}
-          style={{ fontSize: 50, padding: 5 }}
-        >
-          =
+        <Text onPress={() => navigation.toggleDrawer()}>
+          <Ionicons name="menu-outline" size={29} />
         </Text>
-        <Text style={{ padding: 5 }}>Pontech</Text>
+        <Image
+          style={{ width: 40, height: 20 }}
+          source={require("../assets/logoazul.png")}
+        />
       </View>
     </>
   );
@@ -20,10 +21,13 @@ export default function Header({ navigation }) {
 const styles = StyleSheet.create({
   header: {
     paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
     backgroundColor: "#fff",
     flexDirection: "row",
     color: "#000",
     alignItems: "center",
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
   },
 });
