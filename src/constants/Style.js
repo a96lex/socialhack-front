a20ed7e1194fb9e6700ad9b6b8f7460ec39a65fd
a96lex <a96lex@gmail.com>
2021-02-14 03:23:15 +0100

    constants

---
+++ src/constants/Style.js
@@ -0,0 +1,10 @@
+export const DefaultShadow = {
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 6,
+  },
+  shadowOpacity: 0.07,
+  shadowRadius: 7.49,
+  elevation: 5,
+};
---
+++ src/constants/test.js
@@ -0,0 +1,67 @@
+export const actionTest = [
+  {
+    id: 21,
+    Title: "Sweden",
+    description: "works,",
+    nome: "centro1",
+  },
+  {
+    id: 24,
+    Title: "Sweden",
+    description:
+      "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    nome: "centro2",
+  },
+  {
+    id: 23,
+    Title: "Sweden",
+    description:
+      "but also the leap into electronic typesetting, remaining essentially unchanged",
+    nome: "centro3",
+  },
+  {
+    id: 258,
+    Title: "Sweden",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    nome: "centro4",
+  },
+  {
+    id: 26348,
+    Title: "Sweden",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    nome: "centro5",
+  },
+  {
+    id: 524426,
+    Title: "USA",
+    description:
+      "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    nome: "centro6",
+  },
+  {
+    id: 89,
+    Title: "England",
+    description:
+      "but also the leap into electronic typesetting, remaining essentially unchanged",
+    nome: "centro7",
+  },
+];
+
+export const vacas = {
+  ID: "vacas",
+  CEN_NOME: "Asociación de vacas de Pontevedra",
+  CEN_DIRECCION: "C/mooo 33",
+  CEN_CONCELLO: "Pontevedra",
+  CEN_CP: "36005",
+  CEN_TELEFONO: "953 789 023",
+  CEN_EMAIL: "vacas@asociacion.es",
+  CEN_AREA: "Campo",
+  ENT_NOME: "Asociación de vacas",
+  ENT_ANIMO_LUCRO: false,
+  CEN_TIPO: "Albergue",
+  WEB: "vacaspont.es",
+  Descripcion:
+    "Encántannos as vacas e darlles  mimitos, sacalas de paseo e que fagan  tolón-tolón.",
+};
