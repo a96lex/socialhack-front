b696ce779af810e78567a0ba58c314809f46b461
a96lex <a96lex@gmail.com>
2021-02-14 06:32:59 +0100

    test id

--- src/constants/test.js
+++ src/constants/test.js
@@ -1,47 +1,61 @@
 export const actionTest = [
   {
-    id: 21,
+    id: "0_95",
     Title: "Sweden",
     description: "works,",
     nome: "centro1",
   },
   {
-    id: 24,
+    id: "1083_4",
     Title: "Sweden",
     description:
       "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
     nome: "centro2",
   },
   {
-    id: 23,
+    id: "115_3",
     Title: "Sweden",
     description:
       "but also the leap into electronic typesetting, remaining essentially unchanged",
     nome: "centro3",
   },
   {
-    id: 258,
+    id: "23_6",
     Title: "Sweden",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
     nome: "centro4",
   },
   {
-    id: 26348,
+    id: "4168_6",
     Title: "Sweden",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
     nome: "centro5",
   },
   {
-    id: 524426,
+    id: "47_2",
     Title: "USA",
     description:
       "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
     nome: "centro6",
   },
   {
-    id: 89,
+    id: "64_1",
+    Title: "England",
+    description:
+      "but also the leap into electronic typesetting, remaining essentially unchanged",
+    nome: "centro7",
+  },
+  {
+    id: "64_2",
+    Title: "England",
+    description:
+      "but also the leap into electronic typesetting, remaining essentially unchanged",
+    nome: "centro7",
+  },
+  {
+    id: "6650_1",
     Title: "England",
     description:
       "but also the leap into electronic typesetting, remaining essentially unchanged",
